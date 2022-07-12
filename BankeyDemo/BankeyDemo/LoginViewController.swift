fbb47be0208e8dcc5484476d7421accfd44476fc
Arken <Arken.Sarsenov@outlook.com>
2022-07-12 14:24:04 +0500

    Add first onboarding screen

--- BankeyDemo/BankeyDemo/LoginViewController.swift
+++ BankeyDemo/BankeyDemo/LoginViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    let titleLabel = UILabel()
+    let subtitleLabel = UILabel()
     let loginView = LoginView()
     let signInButton = UIButton()
     let errorMessageLabel = UILabel()
@@ -55,6 +57,19 @@ class LoginViewController: UIViewController {
 
 extension LoginViewController {
     private func style() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        titleLabel.adjustsFontSizeToFitWidth = true
+        titleLabel.text = "Bankey"
+        
+        subtitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        subtitleLabel.textAlignment = .center
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        subtitleLabel.adjustsFontForContentSizeCategory = true
+        subtitleLabel.numberOfLines = 0
+        subtitleLabel.text = "Your premium source for all things banking!"
+        
         loginView.translatesAutoresizingMaskIntoConstraints = false
         
         signInButton.translatesAutoresizingMaskIntoConstraints = false
@@ -71,6 +86,17 @@ extension LoginViewController {
     }
     
     private func layout() {
+        
+        //Titels
+        view.addSubview(titleLabel)
+        view.addSubview(subtitleLabel)
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height/4),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            subtitleLabel.topAnchor.constraint(equalToSystemSpacingBelow: titleLabel.bottomAnchor, multiplier: 3),
+            subtitleLabel.leftAnchor.constraint(equalToSystemSpacingAfter: view.leftAnchor, multiplier: 2),
+            view.rightAnchor.constraint(equalToSystemSpacingAfter: subtitleLabel.rightAnchor, multiplier: 2)
+        ])
         
         //Login View
         view.addSubview(loginView)
